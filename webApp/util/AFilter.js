44e1b9c4a23a6d7b31f4355690aae6fdc6a26238
puppetmaster3 <vic@vics-MacBook-Air.local>
2017-01-20 21:49:20 -0500

    Js

--- webApp/util/AFilter.js
+++ webApp/util/AFilter.js
@@ -6,30 +6,36 @@ const SPA = 'spa.html'
 const AMP = 'amp.html'
 const INDEX = 'index.html'
 
-// nvm use 7.4.0 
-// forever start index.js
+function endsWithSlash(str ) {
+	let suffix = '/'
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
 
 exports.filter = function (req, res, next) {
 	if (req.path.indexOf('.') !=-1) { // hasDot?
-		next() // it is static
+		next() // it is static asset, ex: .jpg
 	} else { // no dot, it is a path:
 		try {
 			const pgPath = ROOT + req.path
 			const containsWWWW = (req.subdomains.indexOf('www') > -1) //for subdomain
 			const isWWWW = (req.query.w == '1') || containsWWWW
 			console.log(pgPath + ' ^ ' + isWWWW)
-
-			if (fs.existsSync(pgPath + INDEX)) {// this is not compliant to SPA|AMP
+			if(!endsWithSlash(req.path)) {
+				res.redirect(req.path + '/')
+			} else if (fs.existsSync(pgPath + INDEX)) {// this is not compliant to SPA|AMP
 				console.log(pgPath + INDEX)
 				fs.readFile(pgPath + INDEX, 'utf8', function(err, data) {
+					if(err) throw err
 					res.send(data)
 				})				
-			} else if(isWWWW) //is it SPA/www? 
+			} else if(isWWWW) {//is it SPA/www? 
 				fs.readFile(pgPath + SPA, 'utf8', function(err, data) {
+					if(err) throw err
 					res.send(data)
 				})
-			else { //AMP is default
+			} else { //AMP is default
 				fs.readFile(pgPath + AMP, 'utf8', function(err, data) {
+					if(err) throw err
 					res.send(data)
 				})// readfile
 			} //else AMP
@@ -40,3 +46,4 @@ exports.filter = function (req, res, next) {
 	} // else it is a path
 
 }//()
+
