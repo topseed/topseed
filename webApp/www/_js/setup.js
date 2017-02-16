6a53839c122748dc16d99f24b2056ceece6a48b3
puppetmaster3 <vic@vics-MacBook-Air.local>
2017-02-16 17:07:41 -0500

    Cleaning up

--- webApp/www/_js/setup.js
+++ webApp/www/_js/setup.js
@@ -4,7 +4,7 @@ var _PRE = '_pre-action'
 var _PAGE = '_PAGE'
 var _LOADED = '_loaded'
 var _inAction = false // set to true when user acts; false when effect is done
-var _loaded=false
+var _loaded = false
 
 var _stateA = new signals.Signal()
 var _compA = new signals.Signal()
