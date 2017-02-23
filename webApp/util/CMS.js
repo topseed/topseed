9c8ffecafba41322132204e05765e06469ecd778
puppetmaster3 <vic@vics-MacBook-Air.local>
2017-02-23 15:15:17 -0500

    Images

--- webApp/util/CMS.js
+++ webApp/util/CMS.js
@@ -6,25 +6,38 @@ const router = express.Router()
 // /////////////////////////////////////////////////////
 
 const ROOT = './www/'
+const INFO = '/info.json'
 
 router.post('/dir', function (req, res) {	
 	const dat = req.body
 	console.log(dat)
-	let folder = dat.dir //{ "dir":"post" }
-	folder = ROOT + folder + '/'
+	const dir = dat.dir //{ "dir":"post" }
+
+	const folder = ROOT + dir + '/'
 	console.log(folder)
 	let ff = fs.readdirSync(folder)
 	let ret = new Array()  
 	ff.forEach(function(item){
-		const stats = fs.statSync(folder+item)
-		const d = stats.isDirectory()
-		if(d) {
-			let el = {name: item, date: Date.parse(stats.ctime) }
-			ret.push(el)
-		}
+		try {
+			const stats = fs.statSync(folder+item)
+			const d = stats.isDirectory()
+			if(d) { 
+					const f = folder + item + INFO
+					let el = {}
+					if (fs.existsSync(f)) {
+						el = JSON.parse(fs.readFileSync(f, 'utf8'))
+						//console.log(el)
+					}//fi
+				el['item'] = dir+'/'+item
+				el['time'] = Date.parse(stats.ctime)
+				ret.push(el)
+			}//fi
+		} catch(err) {
+			console.log(err)
+		}// try
 	})
 
-	//console.log(JSON.stringify(ret))
+	//console.log(ret)
 	res.status(200).send(JSON.stringify(ret))
 })// /
 
