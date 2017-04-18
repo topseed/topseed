cfac11b95c46b5fe3eb6019f832dd9ceb0cdd6f3
puppetmaster3 <vicmasons@gmail.com>
2017-04-18 09:05:50 -0400

    re

--- srv/webroot/_js/main.js
+++ srv/webroot/_js/main.js
@@ -23,6 +23,7 @@ loadjs.ready(['dependencyIE', 'keyLibs'], {// loaded setup libs
 function startApp(){
 	// READY ///////////////////////////////////////////////////////////
 	loadjs.done('main')
+	setupDrawer()
 
 	SP.ScontentID ='#content-wrapper'
 	SP.smoothPg.add(function(typ, $new, delta, $html) {
@@ -41,3 +42,21 @@ function startApp(){
 	})
 
 }//startApp()
+
+// /////////////////////////
+function preLImg(arg) { // helper function start loading an image so browser has it ready
+	var imag = new Image()
+	imag.src = arg
+}
+// sidedrawer ////////////////////////////////////////////////
+function setupDrawer() {
+	$('#sidedrawer').on('click', drawerClose)
+	$('#appbar--brand').on('click', drawerOpen)
+	console.log('d set')
+}//()
+function drawerOpen(px, e) {
+	$( '#sidedrawer').css('transform', 'translateX(201px)')
+}//()
+function drawerClose(e) {
+	$( '#sidedrawer').css('transform', 'translateX(0px)')
+}//()
