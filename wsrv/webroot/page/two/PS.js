7a4dd592805bfd96d32f93b9288caf85fba248ed
puppetmaster3 <vicmasons@gmail.com>
2017-04-25 18:31:20 -0400

    bus

--- wsrv/webroot/page/two/PS.js
+++ wsrv/webroot/page/two/PS.js
@@ -1,6 +1,6 @@
 var PS = { //testable pg services class for pg component com, ds/fetch and such
 
-	pgSrv: new signals.Signal()	//loosely coupled
+	pgBus: new signals.Signal()	//loosely coupled
 
 	, init: function( ) {
 		console.log('ps')
