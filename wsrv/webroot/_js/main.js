f5c463c380de47cd45adcfdc64034b48ff1e2a7d
puppetmaster3 <vicmasons@gmail.com>
2017-04-29 21:10:35 -0400

    moving

--- wsrv/webroot/_js/main.js
+++ wsrv/webroot/_js/main.js
@@ -3,7 +3,7 @@ loadjs.ready(['dependencyIE', 'keyLibs'], {// loaded setup libs
 	success: function(){
 
 		loadjs([
-			'//cdn.jsdelivr.net/bacon.js/0.7.1/Bacon.min.js'
+			'//cdn.jsdelivr.net/bacon.js/0.7.1/Bacon.js'
 
 			,'/_js/libJs/jquery.fullpage.min.css'
 			,'/_js/libJs/jquery.fullpage.min.js'
--- wsrv/webroot/comp/compCom/PS.js
+++ wsrv/webroot/comp/compCom/PS.js
@@ -1,6 +1,8 @@
-var PS = { //testable pg services class for pg component com, ds/fetch and such
+var PS = { //testable pg services class for pg component com, ds/fetch, non UI and such
 
-	pgBus: new signals.Signal()	//loosely coupled
+	//pgBus0: new signals.Signal()
+
+	pgBus: new Bacon.Bus() 	//loosely coupled
 
 	, init: function( ) {
 		console.log('ps')
