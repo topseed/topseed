cc309a4fef4728291ae33ffe737bbe0447629223
Wolfgang <wgehner@gmail.com>
2017-05-19 13:14:58 -0700

    Cleanup

--- bsrv/scode/route/ds/BaseFB.js
+++ bsrv/scode/route/ds/BaseFB.js
@@ -21,5 +21,55 @@ class BaseFB {
 		return array
 	}//()
 
+	update(row) { // insert or update automatic, if key included. Return a promise
+		
+ 		//console.log('update.pk:'+row.pk)
+		//console.log('update.url:'+row.url) 
+
+		if (!row.pk) //empty, obtain new pk for insert
+		{
+			console.log('News update, no pk, is insert')
+			const pk = this.fdb.ref().child(this.table).push().key
+			row.pk = pk
+			const ut = Date.now() //set the date to current, better: do in UI
+			row.ts = ut
+		}
+		
+		return this.fdb.ref(this.table+'/'+ row.pk).update(row).then(function(a,b){
+			return row.pk
+		})
+	}//()
+
+	selectList() {//returns all rows sorted backwards by date
+		return this.ref.once('value').then(function(res){
+				const row = res.val()
+				const rows = BaseFB.toArray(row)
+				//console.log(JSON.stringify(rows))
+				//console.log('select list res')
+				return rows
+		})
+	}//()
+
+	select(pk) {
+		return this.ref.child(pk).once('value').then(function(res){
+				const row = res.val()
+				//console.log(JSON.stringify(row))
+				//console.log('select res')
+				return row
+		})		
+	}//()
+
+	delete(row) {
+		console.log('News delete pk:'+row.pk);
+		if (!row.pk)
+			return 0;
+
+		return this.ref.child(row.pk).remove().then(function(res){
+				console.log('item deleted')
+				return 1;
+		})
+	}//()
+		
+
 }
 module.exports = BaseFB 
